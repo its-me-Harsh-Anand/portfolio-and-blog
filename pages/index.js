496ee4c40cf353d133e5e29008ae959f03907888
its-me-Harsh-Anand <harshvivek.787@gmail.com>
2021-11-15 15:33:09 +0530

    front matter setup with gray-matter

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,42 @@
+import fs from 'fs' //it is for server side not for client side
+import path from 'path'
 import Layout from "../components/Layout";
+import matter from "gray-matter"
 
-
-export default function Home() {
+export default function Home({posts}) {
+  console.log(posts)
   return (
     <Layout>Hello world</Layout>
   )
 }
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map(filename =>{
+  const slug = filename.replace('.md', '')
+
+  const markdownWithMeta = fs.readFileSync(
+    path.join('posts', filename), 
+    'utf-8'
+    )
+  
+  const {data: frontmatter} = matter(markdownWithMeta)
+  return {
+    slug,
+    frontmatter,
+  }
+})
+
+  return {
+    props: {
+      posts
+    },
+  }
+}
+
+/*
+fs module is a server side node module but we are importing here in client side, it gives error
+but fs module says, when we use it in function which completely runs on server side, then we can use it
+gray-matter module is use to parse markdown frontmatter file
+*/
