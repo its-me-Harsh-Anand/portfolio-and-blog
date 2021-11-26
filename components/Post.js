d727f1d2ed70c7d4532118c1c990f997c43d46f1
its-me-Harsh-Anand <harshvivek.787@gmail.com>
2021-11-26 13:44:49 +0530

    my first blog added :)

--- components/Post.js
+++ components/Post.js
@@ -4,16 +4,29 @@ import styles from "@/styles/post.module.css";
 import CategoryLabel from "./CategoryLabel";
 
 export default function Post({ post, searchMenu }) {
+
   return (
     <div className={styles.postDiv}>
       {!searchMenu && (
+        
+        //use this when you have original https:// image
         <Image
           src={post.frontmatter.cover_image}
-          alt=""
+          alt="cover image"
           height={420}
           width={600}
           className={styles.postImage}
         />
+        /*
+        <img 
+          src={post.frontmatter.cover_image}
+          alt="cover image"
+          style={{
+            "height" : "200px",
+            "width" : "300px"
+          }}
+        />
+        */
       )}
 
       <div className={styles.dateAndCategory}>
@@ -36,7 +49,7 @@ export default function Post({ post, searchMenu }) {
           <div className={styles.authorImageDiv}>
             <img
               src={post.frontmatter.author_image}
-              alt=""
+              alt="author image"
               className={styles.authorImage}
             />
             <h3 className={styles.authorName}>{post.frontmatter.author}</h3>
