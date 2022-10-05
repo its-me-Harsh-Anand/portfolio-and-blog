5c0101602fd62d2205fce7286a9866060ce0e670
Aman Singh <singh.aman200226@gmail.com>
2022-10-06 01:04:41 +0530

    cards css changed

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -7,22 +7,24 @@ function projectCard({pname, purl, phomepage, pdescription, pdate}) {
 
     return (
         <div className={style.main}>
-            <h3 className={style.date}>{pdate.slice(0, 10).replaceAll("-", " . ")}</h3>
             <h1 className={style.name}>{pname}</h1>
+            <h3 className={style.date}>{pdate.slice(0, 10).replaceAll("-", " . ")}</h3>
             <p className={style.description}>{pdescription || "Awesome project with no description"}</p>
-            <div className={style.links}>
-                <Link href={purl}>
-                    <a href={purl} target="_blank" rel="noreferrer">
-                        Code <FaExternalLinkAlt />
-                    </a>
-                </Link>
-                {
-                    phomepage && <Link href={phomepage}>
-                        <a target="_blank" href={phomepage} rel="noreferrer">
-                            Website <FaExternalLinkAlt />
+            <div className={style.wrapper}>
+                <div className={style.links}>
+                    <Link href={purl}>
+                        <a href={purl} target="_blank" rel="noreferrer">
+                            Code <FaExternalLinkAlt />
                         </a>
-                        </Link>
-                }
+                    </Link>
+                    {
+                        phomepage && <Link href={phomepage}>
+                            <a target="_blank" href={phomepage} rel="noreferrer">
+                                Website <FaExternalLinkAlt />
+                            </a>
+                            </Link>
+                    }
+                </div>
             </div>
         </div>
     )
